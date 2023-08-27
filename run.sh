2d759768ea90c2052059e3fb4adf2dca0adfc97f
Zhe Tang <tangzh6101@gmail.com>
2023-08-27 16:59:56 +0000

    Finish validating the auto-load function of LLVM.

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ SRC=$BASE_PATH/src
 BUILD=$BASE_PATH/build
 LIB=$BASE_PATH/lib
 INCLUDE=$BASE_PATH/include
+LOG=$BASE_PATH/log
 
 JSON_PATH=/home/tz/test_kernel/kernel_source_code/linux-6.2.15/compile_commands.json 
 JSON_PATH=/home/tz/MigrationHint/tmp/tmp.json
@@ -13,5 +14,6 @@ JSON_PATH=/home/tz/MigrationHint/tmp/tmp.json
 pushd $BUILD
     cmake .. -G Ninja
     ninja
-    ./CodeAnalysis ${JSON_PATH} ${SRC}/CodeAnalysis.cpp
+    # ./CodeAnalysis ${JSON_PATH} ${SRC}/CodeAnalysis.cpp
+    ./bin/CodeAnalysis /home/tz/test_kernel/kernel_source_code/linux-6.2.15/mm/zswap.c 2>&1 | tee ${LOG}/`date +%Y%m%d-%H%M%S`.log
 popd
--- src/CodeAnalysis.cpp
+++ src/CodeAnalysis.cpp
@@ -1,62 +1,10 @@
-// #include <llvm/Support/JSON.h>
-// #include <llvm/Support/MemoryBuffer.h>
-// #include <llvm/Support/raw_ostream.h>
-
-// #include <cstdio>
-// #include <fstream>
-// #include <iostream>
-// #include <ostream>
-// #include <string>
-
-// // #include "clang/Tooling/CompilationDatabase.h"
-// #include "clang/Tooling/JSONCompilationDatabase.h"
-// // Clang Findfile
-// // C-API
-// // Include After?
-// // Libtooling?
-
-// int main(int argc, char **argv) {
-//   /*
-//    * Usage:
-//    ** CodeAnalysis [path to compile_commands.json] [path to source file]
-//    */
-//   if (argc != 3) {
-//     std::printf(
-//         "Usage: CodeAnalysis [path to compile_commands.json] [path to source
-//         " "file]\n");
-//     return 1;
-//   }
-//   std::string compile_commands_path = argv[1];
-//   std::string source_file_path = argv[2];
-//   std::cout << compile_commands_path << std::endl;
-//   std::cout << source_file_path << std::endl;
-
-//   // Parse compile_commands.json, whose format begins with json array.
-//   std::string dbErrorMessages;
-//   std::unique_ptr<clang::tooling::JSONCompilationDatabase>
-//   compilation_database(
-//       clang::tooling::JSONCompilationDatabase::loadFromFile(
-//           compile_commands_path, dbErrorMessages,
-//           clang::tooling::JSONCommandLineSyntax::AutoDetect));
-//   auto compile_commands = compilation_database->getAllCompileCommands();
-//   // traverse compile_commands
-//   for (auto &it : compile_commands) {
-//     // std::cout << it.CommandLine << std::endl;
-//     // Output the command line content
-//     for (auto &it2 : it.CommandLine) {
-//       std::cout << it2 << std::endl;
-//     }
-//     // Output the filename and directory
-//     std::cout << it.Filename << std::endl;
-//     std::cout << it.Directory << std::endl << std::endl;
-//   }
-
-//   return 0;
-// }
 #include <clang/Frontend/FrontendActions.h>
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Tooling.h>
 #include <llvm/Support/CommandLine.h>
+
+#include <iostream>
+#include <string>
 // using namespace clang;
 // using namespace clang::tooling;
 // using namespace llvm;
@@ -93,13 +41,56 @@
 static llvm::cl::OptionCategory MyToolCategory("Code-Analysis");
 
 int main(int argc, const char **argv) {
+  //   /*
+  //    * Usage:
+  //    ** CodeAnalysis [path to compile_commands.json] [path to source file]
+  //    */
+  //   if (argc != 3) {
+  //     std::printf(
+  //         "Usage: CodeAnalysis [path to compile_commands.json] [path to
+  //         source " "file]\n");
+  //     return 1;
+  //   }
   llvm::Expected<clang::tooling::CommonOptionsParser> OptionsParser =
       clang::tooling::CommonOptionsParser::create(argc, argv, MyToolCategory,
                                                   llvm::cl::OneOrMore);
-  // Data base can also be imported manually with JSONCompilationDatabase
+  // Database can also be imported manually with JSONCompilationDatabase
   clang::tooling::ClangTool Tool(OptionsParser->getCompilations(),
                                  OptionsParser->getSourcePathList());
 
+#ifdef DEBUG
+  // Validate the compile commands and source file lists.
+  auto compileCommands =
+      OptionsParser->getCompilations().getAllCompileCommands();
+
+  // traverse compile_commands
+  llvm::outs() << "[Debug] Validating compile database: "
+               << "\n";
+  for (auto &it : compileCommands) {
+    // Output the filename and directory
+    llvm::outs() << "[Debug] Filename: " << it.Filename << "\n";
+    llvm::outs() << "[Debug] Directory: " << it.Directory << "\n";
+    // llvm::outs() << it.CommandLine << "\n";
+    // Output the command line content
+    llvm::outs() << "[Debug] Commandline: "
+                 << "\n";
+    for (auto &it2 : it.CommandLine) {
+      llvm::outs() << it2 << "\n";
+    }
+    break;
+  }
+  llvm::outs() << "[Debug] End of compile database."
+               << "\n";
+
+  llvm::outs() << "[Debug] Validating source file lists: "
+               << "\n";
+  auto fileLists = OptionsParser->getSourcePathList();
+  for (auto &it : fileLists) {
+    llvm::outs() << it << "\n";
+  }
+  llvm::outs() << "[Debug] End of source file lists"
+               << "\n";
+#endif
   // return
   // Tool.run(newFrontendActionFactory<HeadFileDependencyAction>().get());
 }
