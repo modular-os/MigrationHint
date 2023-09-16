852e117b6439d49ecd0a7467ab409b800fbe4a50
Zhe Tang <tangzh6101@gmail.com>
2023-09-16 08:11:48 +0000

    Testing: Begin to discomposing the utils functions from the main file.

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,4 +3,31 @@
 #ifndef _CA_UTILS_HPP
 #define _CA_UTILS_HPP
 
-#endif // !_CA_UTILS_HPP
+#include <clang/AST/AST.h>
+// #include <clang/AST/ASTContext.h>
+// #include <clang/AST/Expr.h>
+#include <clang/Basic/SourceLocation.h>
+#include <clang/Basic/SourceManager.h>
+// #include <clang/Frontend/ASTUnit.h>
+// #include <clang/Frontend/CompilerInstance.h>
+// #include <clang/Frontend/FrontendActions.h>
+// #include <clang/Tooling/CommonOptionsParser.h>
+// #include <clang/Tooling/Tooling.h>
+// #include <llvm/Support/CommandLine.h>
+// #include <llvm/Support/raw_ostream.h>
+
+// #include <iostream>
+// #include <map>
+#include <string>
+
+std::string getLocationString(const clang::SourceManager &SM,
+                              const clang::SourceLocation &Loc);
+
+std::string getFuncDeclString(const clang::FunctionDecl *FD);
+
+void printFuncDecl(const clang::FunctionDecl *FD,
+                   const clang::SourceManager &SM);
+
+void printCaller(const clang::CallExpr *CE, const clang::SourceManager &SM);
+
+#endif  // !_CA_UTILS_HPP
---
+++ src/utils.cpp
@@ -0,0 +1,151 @@
+#include <clang/AST/AST.h>
+#include <clang/AST/ASTContext.h>
+#include <clang/AST/Expr.h>
+#include <clang/Basic/SourceLocation.h>
+#include <clang/Basic/SourceManager.h>
+#include <clang/Frontend/ASTUnit.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "utils.hpp"
+
+
+std::string getLocationString(const clang::SourceManager &SM,
+                              const clang::SourceLocation &Loc) {
+  // Get the spelling location for Loc
+  auto SLoc = SM.getSpellingLoc(Loc);
+  std::string FilePath = SM.getFilename(SLoc).str();
+  unsigned LineNumber = SM.getSpellingLineNumber(SLoc);
+  unsigned ColumnNumber = SM.getSpellingColumnNumber(SLoc);
+
+  if (FilePath == "") {
+    // Couldn't get the spelling location, try to get the presumed location
+#if DEBUG
+    llvm::outs << "Couldn't get the spelling location, try to get the presumed "
+                  "location\n";
+#endif
+    auto PLoc = SM.getPresumedLoc(Loc);
+    assert(PLoc.isValid() &&
+           "[getLocationString]: Presumed location in the source file is "
+           "invalid\n");
+    FilePath = PLoc.getFilename();
+    assert(
+        FilePath != "" &&
+        "[getLocationString]: Location string in the source file is invalid.");
+    LineNumber = PLoc.getLine();
+    ColumnNumber = PLoc.getColumn();
+  }
+  return FilePath + ":" + std::to_string(LineNumber) + ":" +
+         std::to_string(ColumnNumber);
+}
+
+std::string getFuncDeclString(const clang::FunctionDecl *FD) {
+  // Return the function declaration string
+  std::string FuncDeclStr;
+  FuncDeclStr =
+      FD->getReturnType().getAsString() + " " + FD->getNameAsString() + "(";
+  if (int paramNum = FD->getNumParams()) {
+    for (auto &it : FD->parameters()) {
+      FuncDeclStr += it->getType().getAsString();
+      if (it->getNameAsString() != "") {
+        FuncDeclStr += " " + it->getNameAsString();
+      }
+      if (--paramNum) {
+        FuncDeclStr += ", ";
+      }
+    }
+  }
+  FuncDeclStr += ")";
+  return FuncDeclStr;
+}
+
+void printFuncDecl(const clang::FunctionDecl *FD,
+                   const clang::SourceManager &SM) {
+  llvm::outs() << "`" << getFuncDeclString(FD) << "`\n";
+  llvm::outs() << "   - Location: `" << getLocationString(SM, FD->getLocation())
+               << "`\n";
+#ifdef DEBUG
+  // Print function with parameters to string FuncDeclStr;
+  std::string FuncDeclStrBuffer, FuncDeclStr;
+  llvm::raw_string_ostream FuncDeclStream(FuncDeclStrBuffer);
+  clang::LangOptions LangOpts;
+  clang::PrintingPolicy PrintPolicy(LangOpts);
+  PrintPolicy.TerseOutput = true;  // 设置为只打印函数签名
+  FD->print(FuncDeclStream, PrintPolicy);
+  FuncDeclStr = FuncDeclStream.str();
+  llvm::outs() << "---Found external function call: " << FuncDeclStr << "\n";
+  FD->getBody()->printPretty(llvm::outs(), nullptr,
+                             clang::PrintingPolicy(clang::LangOptions()));
+#endif
+}
+
+void printCaller(const clang::CallExpr *CE, const clang::SourceManager &SM) {
+  auto CallerLoc = CE->getBeginLoc();
+  auto PLoc = SM.getPresumedLoc(CallerLoc);
+  assert(PLoc.isValid() && "Caller's location in the source file is invalid\n");
+  std::string FilePath = PLoc.getFilename();
+  unsigned LineNumber = PLoc.getLine();
+  unsigned ColumnNumber = PLoc.getColumn();
+
+  llvm::outs() << "`" << FilePath << ":" << LineNumber << ":" << ColumnNumber
+               << "`\n";
+  // Judging whether the caller is expanded from predefined macros.
+  if (SM.isMacroBodyExpansion(CallerLoc)) {
+    auto ExpansionLoc = SM.getImmediateMacroCallerLoc(CallerLoc);
+
+    ExpansionLoc = SM.getTopMacroCallerLoc(ExpansionLoc);
+    PLoc = SM.getPresumedLoc(ExpansionLoc);
+    FilePath = PLoc.getFilename();
+    LineNumber = PLoc.getLine();
+    ColumnNumber = PLoc.getColumn();
+    assert(FilePath != "" &&
+           "(Normal) Macro's original location defined in the header files is "
+           "invalid.");
+  } else if (SM.isMacroArgExpansion(CallerLoc)) {
+#ifdef DEBUG
+    llvm::outs() << "Is in macro arg expansion\n";
+#endif
+    auto ExpansionLoc = SM.getImmediateExpansionRange(CallerLoc).getBegin();
+    FilePath = SM.getFilename(SM.getImmediateSpellingLoc(ExpansionLoc)).str();
+    assert(
+        FilePath != "" &&
+        "(function-like) Macro's original location defined in the header files "
+        "is invalid.");
+    LineNumber = SM.getSpellingLineNumber(ExpansionLoc);
+    ColumnNumber = SM.getSpellingColumnNumber(ExpansionLoc);
+  } else {
+    return;
+  }
+
+#ifdef DEBUG
+  if (SM.isInExternCSystemHeader(CallerLoc)) {
+    llvm::outs() << "Is in system header\n";
+  }
+  if (SM.isWrittenInBuiltinFile(CallerLoc)) {
+    llvm::outs() << "Is in builtin file\n";
+  }
+  if (SM.isInSystemHeader(CallerLoc)) {
+    llvm::outs() << "Is in system header\n";
+  }
+  if (SM.isMacroArgExpansion(CallerLoc) or SM.isMacroBodyExpansion(CallerLoc) or
+      SM.isInSystemMacro(CallerLoc)) {
+    llvm::outs() << SM.isMacroBodyExpansion(CallerLoc) << " "
+                 << SM.isMacroArgExpansion(CallerLoc) << " "
+                 << SM.isInSystemMacro(CallerLoc) << " "
+                 << "Is in macro arg expansion\n";
+  }
+#endif
+
+  llvm::outs() << "         - Expanded from Macro, Macro's definition: `"
+               << FilePath << ":" << LineNumber << ":" << ColumnNumber << "`\n";
+}
