8a4847d614c0d8c88779adee478bd90f83571ff6
Zhe Tang <tangzh6101@gmail.com>
2023-09-16 08:39:49 +0000

    Remove the unneeded comments.

--- include/utils.hpp
+++ include/utils.hpp
@@ -4,20 +4,9 @@
 #define _CA_UTILS_HPP
 
 #include <clang/AST/AST.h>
-// #include <clang/AST/ASTContext.h>
-// #include <clang/AST/Expr.h>
 #include <clang/Basic/SourceLocation.h>
 #include <clang/Basic/SourceManager.h>
-// #include <clang/Frontend/ASTUnit.h>
-// #include <clang/Frontend/CompilerInstance.h>
-// #include <clang/Frontend/FrontendActions.h>
-// #include <clang/Tooling/CommonOptionsParser.h>
-// #include <clang/Tooling/Tooling.h>
-// #include <llvm/Support/CommandLine.h>
-// #include <llvm/Support/raw_ostream.h>
 
-// #include <iostream>
-// #include <map>
 #include <string>
 
 std::string getLocationString(const clang::SourceManager &SM,
