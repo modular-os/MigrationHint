9e9b6759e4aeaac45a0fc3c379cae08a554e9000
Zhe Tang <tangzh6101@gmail.com>
2023-10-16 16:36:23 +0800

    Refactor the source code stucture, decomposing preprocessor callbacks from the main file.

---
+++ include/ca_PreprocessorHelpers.hpp
@@ -0,0 +1,182 @@
+#pragma once
+
+#ifndef _CA_PREPROCESSOR_HELPERS_HPP
+#define _CA_PREPROCESSOR_HELPERS_HPP
+
+// #include <clang/AST/AST.h>
+// #include <clang/AST/ASTContext.h>
+// #include <clang/AST/Decl.h>
+// #include <clang/AST/Expr.h>
+// #include <clang/AST/OperationKinds.h>
+#include <clang/Basic/SourceLocation.h>
+// #include <clang/Basic/SourceManager.h>
+// #include <clang/Frontend/ASTUnit.h>
+#include <clang/Frontend/CompilerInstance.h>
+// #include <clang/Frontend/FrontendAction.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Lex/PPCallbacks.h>
+#include <clang/Lex/Preprocessor.h>
+// #include <clang/Tooling/CommonOptionsParser.h>
+// #include <clang/Tooling/Tooling.h>
+// #include <llvm/Support/CommandLine.h>
+// #include <llvm/Support/raw_ostream.h>
+
+// #include <iostream>
+// #include <map>
+// #include <string>
+// #include <vector>
+
+#include "clang/AST/RecursiveASTVisitor.h"
+// #include "clang/ASTMatchers/ASTMatchFinder.h"
+// #include "clang/ASTMatchers/ASTMatchers.h"
+// #include "ca_utils.hpp"
+#include "clang/Lex/MacroArgs.h"
+// #include "ca_PreprocessorHelpers.hpp"
+// #include "ca_ASTHelpers.hpp"
+namespace ca {
+/**********************************************************************
+ * 1. Preprocessor Callbacks
+ **********************************************************************/
+class MacroPPCallbacks : public clang::PPCallbacks {
+ public:
+  explicit inline MacroPPCallbacks(const clang::CompilerInstance &compiler)
+      : compiler(compiler), MacroCounts(0), HeaderCounts(0) {
+    name = compiler.getSourceManager()
+               .getFileEntryForID(compiler.getSourceManager().getMainFileID())
+               ->getName();
+  }
+
+ private:
+  int getMacroExpansionStackDepth(std::string MacroName,
+                                  std::string MacroString,
+                                  const clang::MacroArgs *Args,
+                                  const clang::Preprocessor &PP);
+//                                    {
+//     std::vector<std::string> CurrMacro;
+//     CurrMacro.push_back(MacroName);
+//     CurrMacro.push_back(MacroString);
+//     if (Args) {
+// #ifdef DEBUG
+//       llvm::outs() << Args->getNumMacroArguments() << "\n";
+// #endif
+//       for (unsigned I = 0, E = Args->getNumMacroArguments(); I != E; ++I) {
+// #ifdef DEPRECATED
+//         // Output the expanded args for macro, may depend on the following
+//         // expansion of other macros.
+//         const auto &Arg =
+//             const_cast<clang::MacroArgs *>(Args)->getPreExpArgument(I, PP);
+//         // Traverse the Args
+//         llvm::outs() << "Argument " << I << ": ";
+//         for (auto &it : Arg) {
+//           llvm::outs() << PP.getSpelling(it) << " ";
+//         }
+//         llvm::outs() << "\n";
+// #endif
+//         const auto Arg = Args->getUnexpArgument(I);
+// #ifdef DEBUG
+//         llvm::outs() << "Argument " << I << ": " << PP.getSpelling(*Arg)
+//                      << "\n";
+// #endif
+//         CurrMacro.push_back(PP.getSpelling(*Arg));
+//       }
+//     }
+
+//     if (MacroExpansionStack.size() == 0) {
+//       MacroExpansionStack.push_back(CurrMacro);
+//       return 0;
+//     }
+//     int flag = 0;
+//     while (MacroExpansionStack.size()) {
+//       for (auto MacroPart : MacroExpansionStack.back()) {
+//         if (MacroPart.find(MacroName) != std::string::npos) {
+//           if (MacroPart == MacroExpansionStack.back().front() &&
+//               MacroPart == MacroName) {
+//             // If the currentMacro is identical to the macros in stack, return.
+//             return MacroExpansionStack.size() - 1;
+//           }
+//           flag = 1;
+//           break;
+//         }
+//       }
+//       if (flag) {
+//         break;
+//       } else {
+//         MacroExpansionStack.pop_back();
+//       }
+//     }
+//     MacroExpansionStack.push_back(CurrMacro);
+//     return MacroExpansionStack.size() - 1;
+//   }
+
+ public:
+  void InclusionDirective(clang::SourceLocation HashLoc,
+                          const clang::Token &IncludeTok,
+                          clang::StringRef FileName, bool IsAngled,
+                          clang::CharSourceRange FilenameRange,
+                          clang::OptionalFileEntryRef File,
+                          clang::StringRef SearchPath,
+                          clang::StringRef RelativePath,
+                          const clang::Module *Imported,
+                          clang::SrcMgr::CharacteristicKind FileType) override;
+//                            {
+//     auto &SM = compiler.getSourceManager();
+//     if (SM.isInMainFile(HashLoc)) {
+//       if (HeaderCounts == 0) {
+//         llvm::outs() << "# Header File: \n";
+//       }
+//       llvm::outs() << ++HeaderCounts << ". `"
+//                    << ca_utils::getLocationString(SM, HashLoc) << "`: `"
+//                    << SearchPath.str() + "/" + RelativePath.str() << "`\n";
+//     }
+//   }
+
+  void MacroExpands(const clang::Token &MacroNameTok,
+                    const clang::MacroDefinition &MD, clang::SourceRange Range,
+                    const clang::MacroArgs *Args) override ;
+//                     {
+//     auto &SM = compiler.getSourceManager();
+//     auto &PP = compiler.getPreprocessor();
+//     const clang::LangOptions &LO = PP.getLangOpts();
+//     if (SM.isInMainFile(Range.getBegin())) {
+//       auto MacroDefinition = ca_utils::getMacroDeclString(MD, SM, LO);
+//       int MacroDepth = getMacroExpansionStackDepth(PP.getSpelling(MacroNameTok),
+//                                                    MacroDefinition, Args, PP);
+//       if (MacroCounts == 0) {
+//         llvm::outs() << "# Macro Expansion Analysis: \n";
+//       }
+//       if (MacroDepth == 0) {
+//         llvm::outs() << "```\n\n## Macro " << ++MacroCounts << ": \n```\n";
+//       }
+//       llvm::outs().indent(MacroDepth * 3)
+//           << PP.getSpelling(MacroNameTok) << ", "
+//           << ca_utils::getLocationString(SM, Range.getBegin()) << "\n";
+//     }
+//   }
+
+ private:
+  const clang::CompilerInstance &compiler;
+  std::string name;
+
+  std::vector<std::vector<std::string>> MacroExpansionStack;
+  int MacroCounts;
+  int HeaderCounts;
+};
+class MacroPPOnlyAction : public clang::PreprocessOnlyAction {
+#ifdef DEPRECATED
+  // The following code is deprecated temporarily. May be useful in the
+  // future.
+  virtual bool BeginSourceFileAction(CompilerInstance &CI) { return true; }
+  virtual void EndSourceFileAction() {}
+#endif
+
+ protected:
+  void ExecuteAction() override;
+//    {
+//     getCompilerInstance().getPreprocessor().addPPCallbacks(
+//         std::make_unique<MacroPPCallbacks>(getCompilerInstance()));
+
+//     clang::PreprocessOnlyAction::ExecuteAction();
+//   }
+};
+}  // namespace ca
+#endif  // !_CA_PREPROCESSOR_HELPERS_HPP
