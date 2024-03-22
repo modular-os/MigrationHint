b5801e2679cd1d7d49af67ca1a4497aa3fee4c5e
NakanoMiku <jinleho@hotmail.com>
2024-03-22 15:36:56 +0800

    Add support to JSON file parsing

--- src/CodeGen/src/cg_main.cpp
+++ src/CodeGen/src/cg_main.cpp
@@ -1,6 +1,10 @@
 #include "llvm/Support/JSON.h"
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/MemoryBuffer.h>
+
+#include <iostream>
+#include <fstream>
 
 using namespace llvm;
 using namespace std;
@@ -10,7 +14,30 @@ static cl::opt<string> outputFile("o", cl::Positional, cl::desc("Name of output 
 
 int main(int argc, const char **argv) {
   cl::ParseCommandLineOptions(argc, argv);
-  llvm::outs() << "test" <<  jsonFile;
+  outs() << "test" <<  jsonFile;
 
+  // Read from JSON to buffer
+  auto BufferOrErr = llvm::MemoryBuffer::getFile(jsonFile);
+  llvm::StringRef Buffer = (*BufferOrErr)->getBuffer();
+  llvm::Expected<llvm::json::Value> Parsed = llvm::json::parse(Buffer);
+
+  if (!Parsed) {
+    // 解析失败，处理错误
+    auto Err = Parsed.takeError();
+
+    // 输出错误信息
+    llvm::errs() << "Failed to parse JSON: ";
+    llvm::handleAllErrors(std::move(Err), [&](const llvm::ErrorInfoBase &EIB) {
+      llvm::errs() << EIB.message() << "\n";
+    
+    });
+    return 1;
+  
+  }
+
+  // Convert value type into array type
+  llvm::json::Array* JSONRoot = Parsed.get().getAsArray();
+  outs() << *JSONRoot->front().getAsString();
+  outs() <<"passed";
 }
 
